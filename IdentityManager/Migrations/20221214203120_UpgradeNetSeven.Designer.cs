255067e884d6bf846d9edf85c7e427a7f33725cb
Youssef Wael Mohamed-Lotfy <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-12-14 23:18:25 +0200

    Upgrade to .NET 7 (#1)
    * Updated Packages and removed old Startup style
    
    * Fixed EF Migration

--- IdentityManager/Migrations/20221214203120_UpgradeNetSeven.Designer.cs
+++ IdentityManager/Migrations/20221214203120_UpgradeNetSeven.Designer.cs
@@ -4,6 +4,7 @@ using IdentityManager.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IdentityManager.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221214203120_UpgradeNetSeven")]
+    partial class UpgradeNetSeven
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
